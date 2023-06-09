631987cb0b49c8a819dc63ad3d8904aed26c14b1
Parking Master <parkingmaster@email.com>
2023-06-09 10:40:55 -0400

    Finish for today

--- scripts/LevelMapping.js
+++ scripts/LevelMapping.js
@@ -1,7 +1,7 @@
 (function() {
   let levels = [];
   LevelMapping = {};
-  for (let i = 0; i < 25; i++) {
+  for (let i = 0; i < 12; i++) {
     if (i % 3 === 0 && i != 0) {
       levels.push("bonus/bonus" + ((i / 3) - 1));
     }
@@ -12,4 +12,6 @@
   let nextNextLevel = levels[levels.indexOf(location.pathname.replace(".html", "").split("/").splice(2).join("/")) + 2];
   LevelMapping.nextLevel = `/levels/${nextLevel}.html`;
   LevelMapping.nextNextLevel = `/levels/${nextNextLevel}.html`;
+  LevelMapping.allLevels = levels;
+  LevelMapping.currentLevel = LevelMapping.allLevels.indexOf(location.pathname.split("/").pop().split(".")[0]) + 1;
 })();
